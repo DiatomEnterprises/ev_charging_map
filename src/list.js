0094118cda5fee2659ada7a856da0bd1ce227467
Alex <1sasha.sasha1@gmail.com>
2018-09-17 15:42:10 +0200

    Changed font, styled list view

--- src/list.js
+++ src/list.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './css/list.css';
 
 class List extends React.Component {
   render(){
     return(
-      <div className="static">
+      <div className="static list-info">
         {this.props.markers.map(marker => (
-          <div key={marker.id}>
-            <h3>{marker.AddressInfo.Title} - {marker.AddressInfo.UsageCost || "$0.00" } - {marker.NumberOfPoints || "?"} points</h3>
-            <p>lat: {marker.AddressInfo.Latitude}, lng: {marker.AddressInfo.Longitude}</p>
-            <p>Comments: {marker.GeneralComments}</p>
+          <div key={marker.id} className="list-item">
+            <h3 className="list-title">{marker.AddressInfo.Title} - {marker.NumberOfPoints || "?"} charging points <div className="right">{marker.AddressInfo.UsageCost || "$0.00" }</div></h3>
+            <p><i className="fas fa-map-marker-alt" /> lat: {marker.AddressInfo.Latitude}, lng: {marker.AddressInfo.Longitude} <a href={"https://www.google.com/maps/dir/?api=1&destination="+marker.AddressInfo.Latitude+","+marker.AddressInfo.Longitude} target="_blank"> Navigate! </a></p>
+            <p><i className="fas fa-comments" /> Comments: {marker.GeneralComments}</p>
             <p>Accessibility Comments: {marker.AddressInfo.AccessComments}</p>
           </div>))}
       </div>
